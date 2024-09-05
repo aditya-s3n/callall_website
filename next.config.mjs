aa98b6aa1e0f9d8384f39cbc4f707277ef67ce8d
Aditya Sen <adityasen120@gmail.com>
2024-09-05 03:47:40 +0100

    first mock-up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['callall.ai'], // Add your domain here
+    },
+};
 
 export default nextConfig;
