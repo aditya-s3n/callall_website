39c58947ea7a5be1e80b11defc3ebf7a860ee6a2
Aditya Sen <a5sen@uwaterloo.ca>
2024-10-07 18:46:04 -0400

    add /calculator directory

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Calculator from "../components/Calculator";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../css/landing.css";
+
+function CalculatorPage() {
+    return (
+        <div className="">
+            <Header />
+            
+            <div className="text-center my-5">
+                <p className="fw-bold fs-2 mb-2">Calculate Potential Earnings of your Leads</p>
+
+                <p className="header-link nav-link fs-5">Plug in a few numbers and gain visibility on the value of your database</p>
+            </div>
+
+
+            <div className="m-auto section-width">
+                <Calculator />
+            </div>
+            
+
+            <Footer />
+        </div>
+    )
+}
+
+export default CalculatorPage;
